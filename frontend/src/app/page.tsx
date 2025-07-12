c58bbab9abb0f38eb31cad34099de30ca6659a45
Aditya-Raj143 <adityaxraj72004@gmail.com>
2025-07-12 10:14:55 +0530

    added skill cards

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,9 +1,23 @@
+import SkillCard from '@/components/cards/CardComponent';
 import Navbar from '@/components/Navbar';
+import { skillCardsData } from '@/constants/skillCardData';
 
 export default function Home() {
   return (
-    <div>
+    <div className="min-h-screen text-white">
       <Navbar />
+      <div className="flex flex-col items-center justify-center gap-6 py-8">
+        {skillCardsData.map((card, index) => (
+          <SkillCard
+            key={index}
+            name={card.name}
+            skillsOffered={card.skillsOffered}
+            skillsWanted={card.skillsWanted}
+            rating={card.rating}
+            profilePhotoUrl={card.profilePhotoUrl}
+          />
+        ))}
+      </div>
     </div>
   );
 }
