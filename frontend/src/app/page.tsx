2e40c52c3cc79341f623113f57d7fb4f3b6a21d8
saatvik-10 <saatvikmadan529@gmail.com>
2025-07-12 09:15:14 +0530

    initialized repo

---
+++ frontend/src/app/page.tsx
@@ -0,0 +1,10 @@
+import { Button } from '@/components/ui/button';
+
+export default function Home() {
+  return (
+    <div>
+      Template by Saatvik
+      <Button className='bg-red-500 text-white'>Click</Button>
+    </div>
+  );
+}
